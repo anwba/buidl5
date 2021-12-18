2e2f519c52f177d3ea5cc84cdcab7b17e641e92f
anwba <an@wbookapp.com>
2021-12-18 09:15:39 +0700

    back to db access

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -21,7 +21,7 @@ export default function Index() {
         <nav>
           <ul>
             <li>
-              <Link to="zokes">Read Jokes</Link>
+              <Link to="jokes">Read Jokes</Link>
             </li>
           </ul>
         </nav>
